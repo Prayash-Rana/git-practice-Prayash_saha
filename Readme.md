d712e1c21180952bf94785bca9a9440a29b7ad88
Prayash Saha <prayashaust@gmail.com>
2026-04-30 23:14:10 +0600

    Initial commit: project structure and basic program

---
+++ Readme.md
@@ -0,0 +1,5 @@
+Project Tiile : AI Integration on Daily Life
+
+name  : Prayash Saha
+
+objecive :  this project will reduce the daily repeated tasks  
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+name = "Prayash saha"
+date = datetime.now().strftime("%Y-%m-%d")
+
+
+print(f" User name : {name} ")
+
+print(f"Today's date: {date}")
