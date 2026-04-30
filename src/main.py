ff7bc1f3577f1ff8d5215ec590fca153c29eb66c
Prayash Saha <prayashaust@gmail.com>
2026-05-01 00:20:41 +0600

    error handing of calculatorfunctions

--- src/main.py
+++ src/main.py
@@ -9,10 +9,10 @@ print(f" User name : {name} ")
 print(f"Today's date: {date}")
 
 
-sum_result = add(10, 5)
+sum_result = add(10, 20)
 print(f"sum of 10 and 5 is: {sum_result}")
 
-# Calling subtract function
+
 subtract_result = subtract(10, 5)
 print(f"subtract of 10 and 5 is: {subtract_result}")
 
--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,23 @@
 
 def add(a, b):
+    if type(a) not in (int, float) or type(b) not in (int, float):
+        return "Error: Both a and b must be numbers"
     return a + b
 
 
 def subtract(a, b):
+    if type(a) not in (int, float) or type(b) not in (int, float):
+        return "Error: Both a and b must be numbers"
     return a - b
 
 def multiply(a, b):
+    if type(a) not in (int, float) or type(b) not in (int, float):
+        return "Error: Both a and b must be numbers"
     return a * b
 
 def divide(a, b):
+    if type(a) not in (int, float) or type(b) not in (int, float):
+        return "Error: Both a and b must be numbers"
     if b==0:
         return "Error:  Can not divide by zero"
     return a / b
