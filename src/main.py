72ea7dae463cd3d934103e95af1cdb967e3f79ae
Prayash Saha <prayashaust@gmail.com>
2026-04-30 23:36:58 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,11 +1,21 @@
+
 from datetime import datetime
+from utils import add, subtract
 
 name = "Prayash saha"
 date = datetime.now().strftime("%Y-%m-%d")
 
-
 print(f" User name : {name} ")
-
 print(f"Today's date: {date}")
+
+
+sum_result = add(10, 5)
+print(f"sum of 10 and 5 is: {sum_result}")
+
+# Calling subtract function
+subtract_result = subtract(10, 5)
+print(f"subtract of 10 and 5 is: {subtract_result}")
+
+
 
 
