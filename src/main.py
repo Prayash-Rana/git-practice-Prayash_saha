9a15a452b77e6b15cdfc17ff35afd60a24f23f54
Prayash Saha <prayashaust@gmail.com>
2026-05-01 00:35:11 +0600

    apply multiply and divide functions at main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 
 from datetime import datetime
-from utils import add, subtract
+from utils import add, subtract, multiply, divide
 
 name = "Prayash saha"
 date = datetime.now().strftime("%Y-%m-%d")
@@ -8,13 +8,17 @@ date = datetime.now().strftime("%Y-%m-%d")
 print(f" User name : {name} ")
 print(f"Today's date: {date}")
 
-
 sum_result = add(10, 20)
 print(f"sum of 10 and 5 is: {sum_result}")
 
-
 subtract_result = subtract(10, 5)
 print(f"subtract of 10 and 5 is: {subtract_result}")
+
+multiply_result = multiply(10, 5)
+print(f"multiply of 10 and 5 is: {multiply_result}")
+
+divide_result = divide(10, 5)
+print(f"divide of 10 and 5 is: {divide_result}")
 
 
 
